96d69a0aea07a1451715e5d2f0204e3c4accdd1e
bigkai <1320167011@qq.com>
2020-06-11 00:16:35 +0800

    feign + 熔断 + 熔断仪表盘

--- hello-spring-cloud-web-admin-ribbon/src/main/java/com/funtl/hello/spring/cloud/web/admin/ribbon/WebAdminRibbonApplication.java
+++ hello-spring-cloud-web-admin-ribbon/src/main/java/com/funtl/hello/spring/cloud/web/admin/ribbon/WebAdminRibbonApplication.java
@@ -3,9 +3,11 @@ package com.funtl.hello.spring.cloud.web.admin.ribbon;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @EnableEurekaClient
 @SpringBootApplication
+@EnableHystrix
 public class WebAdminRibbonApplication {
     public static void main(String[] args) {
         SpringApplication.run(WebAdminRibbonApplication.class, args);
--- hello-spring-cloud-web-admin-ribbon/src/main/java/com/funtl/hello/spring/cloud/web/admin/ribbon/service/AdminService.java
+++ hello-spring-cloud-web-admin-ribbon/src/main/java/com/funtl/hello/spring/cloud/web/admin/ribbon/service/AdminService.java
@@ -1,5 +1,6 @@
 package com.funtl.hello.spring.cloud.web.admin.ribbon.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -10,7 +11,12 @@ public class AdminService {
     @Autowired
     private RestTemplate restTemplate;
 
+    @HystrixCommand(fallbackMethod = "sayErr")
     public String sayHi(String message) {
         return restTemplate.getForObject("http://hello-spring-cloud-service-admin/hi?message=" + message, String.class);
+    }
+
+    private String sayErr(String message) {
+        return String.format("Hi,your message is : %s", message);
     }
 }
